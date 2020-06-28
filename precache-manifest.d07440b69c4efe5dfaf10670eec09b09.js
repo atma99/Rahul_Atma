5a932a0d2a6c9cf7e50427a5dec2536fc830a088
atma99 <@rahula830@gmail.com>
2020-06-28 01:49:11 -0400

    Updates

---
+++ precache-manifest.d07440b69c4efe5dfaf10670eec09b09.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c447db65e03f15f0de30198195b5a353",
+    "url": "/Rahul_Atma/index.html"
+  },
+  {
+    "revision": "ab7e3f69a256054daecb",
+    "url": "/Rahul_Atma/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "59fda12f05addb4e1698",
+    "url": "/Rahul_Atma/static/css/main.2037451b.chunk.css"
+  },
+  {
+    "revision": "ab7e3f69a256054daecb",
+    "url": "/Rahul_Atma/static/js/2.4a7f211d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Rahul_Atma/static/js/2.4a7f211d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59fda12f05addb4e1698",
+    "url": "/Rahul_Atma/static/js/main.bc98409e.chunk.js"
+  },
+  {
+    "revision": "0e444351d83dbe887735",
+    "url": "/Rahul_Atma/static/js/runtime-main.0152e861.js"
+  },
+  {
+    "revision": "6c271d98d08f06e06b19c2f2d9dfa6c3",
+    "url": "/Rahul_Atma/static/media/Chartwell-Logo.6c271d98.jpg"
+  },
+  {
+    "revision": "d69e07e475de327d8726704b80f36aef",
+    "url": "/Rahul_Atma/static/media/Pratt-logo.d69e07e4.png"
+  },
+  {
+    "revision": "16c34fb554b120bef302969bf3a5532b",
+    "url": "/Rahul_Atma/static/media/martletLogo.16c34fb5.png"
+  },
+  {
+    "revision": "5ed6cb65757f1abee2fee564602e9dca",
+    "url": "/Rahul_Atma/static/media/nbalogo.5ed6cb65.jpg"
+  },
+  {
+    "revision": "13465d54f73ebffbfd88997813d17a7e",
+    "url": "/Rahul_Atma/static/media/primeAIDpic3.13465d54.png"
+  },
+  {
+    "revision": "fa9c39b94674d35e3af286ca731e4983",
+    "url": "/Rahul_Atma/static/media/profile.fa9c39b9.png"
+  }
+]);
